63e97edeea178944a3c1f6aa4fb0d317b1e07167
William Dawn <wcdawn@ncsu.edu>
2018-09-24 11:57:29 -0400

    finished 1dfixed and 1d1g

--- apA_analyticSolutions/matlab/fixed_critical_1d1g.m
+++ apA_analyticSolutions/matlab/fixed_critical_1d1g.m
@@ -4,14 +4,17 @@ close all
 Lx = 1;
 ntest = 100;
 
+diffusion = 1;
 qfixed = 1;
 sigma_r = 1;
 nusigma_f = 2;
 
+Bf = sqrt(sigma_r/diffusion);
+
 xtest = linspace(0,Lx,ntest);
 
-fixed = @(x)((qfixed/sigma_r)*(1-cosh(sigma_r*x))+ ...
-    ((-qfixed/sigma_r)*(1-cosh(sigma_r*Lx))/(sinh(sigma_r*Lx)))*sinh(sigma_r*x));
+fixed = @(x)((qfixed/Bf^2)*(1-cosh(Bf*x))+ ...
+    ((qfixed/Bf^2)*(cosh(Bf*Lx)-1)/(sinh(Bf*Lx)))*sinh(Bf*x));
 critical = @(x)(sin((pi/Lx)*x));
 
 ytestf = zeros(size(xtest));
@@ -25,7 +28,7 @@ ytestc = ytestc * norm(ytestf,inf);
 figure
 hold on
 title('One-Dimension, One-Group, Fixed and Critical')
-plot(xtest,ytestc)
-plot(xtest,ytestf)
+plot(xtest,ytestc,'LineWidth',2)
+plot(xtest,ytestf,'LineWidth',2)
 legend({'Fixed Source', 'Criticality'})
 hold off
