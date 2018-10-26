806f8792c6ec95b27cc79951214c412a47eb3ce8
William Dawn <wcdawn@ncsu.edu>
2018-10-26 13:30:00 -0400

    text is there. cleanup & send off.

---
+++ ch04_thermalHydraulics/figs/matlab/axial_temp_plot.m
@@ -0,0 +1,32 @@
+clear
+close all
+
+fname = '~/dev/thesis/sfr/fort.999';
+
+LW = 2;  % line width
+FS = 13; % font size
+
+data = csvread(fname);
+z = data(:,1);
+enthalpy = data(:,2);
+tcool = data(:,3);
+tclad = data(:,4);
+tbond = data(:,5);
+tfuel = data(:,6);
+
+figure
+hold on
+h = plot(tcool,z,'-','LineWidth',LW);
+set(h, 'MarkerFaceColor', get(h,'Color'));
+h = plot(tclad,z,'-','LineWidth',LW);
+set(h, 'MarkerFaceColor', get(h,'Color'));
+h = plot(tbond,z,'-','LineWidth',LW);
+set(h, 'MarkerFaceColor', get(h,'Color'));
+h = plot(tfuel,z,'-','LineWidth',LW);
+set(h, 'MarkerFaceColor', get(h,'Color'));
+hold off
+legend_labels = {'T_{cool}','T_{clad}','T_{bond}','T_{fuel}'};
+legend(legend_labels,'Location','NorthWest');
+ylabel('Elevation [cm]')
+xlabel('Temperature [K]')
+title('Typical Axial Temperature Plot')
