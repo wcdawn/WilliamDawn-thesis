8a00562e8a0414883137cdd8b4d3a5ca1fd5889d
William Dawn <wcdawn@ncsu.edu>
2018-11-14 15:18:15 -0500

    adding LEF figures and citations

---
+++ ch05_thermalExpansion/matlab/lef_plot.m
@@ -0,0 +1,32 @@
+clear
+close all
+
+fname = '~/dev/thesis/sfr/lef.csv';
+data = csvread(fname,1,0);
+LW = 2;
+
+T = data(:,1);
+lef_fuel  = data(:,2);
+lef_steel = data(:,3);
+
+ndot = 10;
+ntest = length(T);
+delta_selected = ntest/ndot;
+selected = floor(1:delta_selected:ntest);
+
+matlab_blue = [0.00 0.447 0.741];
+matlab_orange = [0.91 0.41 0.17];
+
+figure
+hold on
+plot(T,lef_fuel ,'-o','Color',matlab_blue,'LineWidth',LW,'MarkerIndices',selected,'MarkerFaceColor',matlab_blue)
+plot(T,lef_steel,'-^','Color',matlab_orange,'LineWidth',LW,'MarkerIndices',selected,'MarkerFaceColor',matlab_orange)
+hold off
+legend({'LEF U10Zr','LEF HT9'},'Location','NorthWest')
+xlabel('Temperature [K]')
+ylabel('Linear Expansion Factor (LEF)')
+xlim([T(1),T(end)])
+ylim([0,max(lef_fuel)*1.05])
+ax = gca;
+ax.YAxis.Exponent=-2; % this is the same as percentage results
+print(gcf,'../figs/lef_plot.png','-dpng','-r800');
