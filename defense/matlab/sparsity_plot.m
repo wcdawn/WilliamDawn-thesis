cf6f270a4112cad2edc4c2cc2fe2aba3adb77307
William Dawn <wcdawn@ncsu.edu>
2019-01-12 17:43:44 -0500

    created figures

--- defense/matlab/sparsity_plot.m
+++ defense/matlab/sparsity_plot.m
@@ -1,8 +1,8 @@
 clear
 close all
 
-fname_uno = 'uno.csv';
-fname_rcm = 'rcm.csv';
+fname_uno = '../data/uno.csv';
+fname_rcm = '../data/rcm.csv';
 
 data_uno = csvread(fname_uno);
 data_rcm = csvread(fname_rcm);
@@ -12,14 +12,16 @@ A_rcm = sparse(data_rcm(:,1),data_rcm(:,2),data_rcm(:,3));
 
 figure
 spy(A_uno)
-title('Dense Pattern')
+title_str = sprintf('Unordered Pattern - Bandwidth %d',bandwidth(A_uno));
+title(title_str)
 set(gca,'FontName','Times New Roman','FontSize',12);
 print('../figs/uno_pattern.eps','-depsc2');
-%close(gcf)
+close(gcf)
 
 figure
 spy(A_rcm)
-title('Sparse Pattern')
+title_str = sprintf('RCM Pattern - Bandwidth %d',bandwidth(A_rcm));
+title(title_str)
 set(gca,'FontName','Times New Roman','FontSize',12);
 print('../figs/rcm_pattern.eps','-depsc2');
-%close(gcf)
+close(gcf)
