629ea12d73864646b78fdd09c1547aec88de2dc3
William Dawn <wcdawn@ncsu.edu>
2019-01-24 08:36:41 -0500

    added reactivity coefficient figures

--- ch06_coupledResults/matlab/reactivity_coeff_plot.m
+++ ch06_coupledResults/matlab/reactivity_coeff_plot.m
@@ -1,30 +1,50 @@
 clear
 close all
 
-fname = '../data/reactivity_study_01012019_bak.csv';
+fname = '../data/reactivity_study.csv';
+LW = 2;
 FN = 'Times New Roman';
 FS = 12;
-LW = 2;
-fuelfudge = 20.;
-coolfudge = 50.;
+fuelfudge = 20.; % [K]
+coolfudge = 50.; % [K]
+powerfudge = 10.; % [%]
 
-% [power, all feedback, no t.e., fuelfudge, coolfudge]
+% [power, tfuel_avg, tclad_avg, tcool_avg, k_ref, k_perturb_power,
+%    k_perturb_thexp, fuelfudge, coolfudge]
 data = csvread(fname);
 
 % parse data
 nPower = size(data,1);
-nCondition = size(data,2)-1;
+nCondition = size(data,2)-4;
 power = data(:,1);
-keff = data(:,2:nCondition+1);
+temperature_avg = data(:,2:4);
+keff = data(:,5:size(data,2));
 
 % calculate reactivity
 reac = zeros(nPower,nCondition);
 for i = 1:nPower
     for j = 1:nCondition
-        reac(i,j) = (data(i,j+1)-data(1,j+1))/(data(i,j+1)*data(1,j+1));
+        %reac(i,j) = (data(i,j+1)-data(1,j+1))/(data(i,j+1)*data(1,j+1));
+        reac(i,j) = (keff(i,j) - 1.0)/keff(i,j);
     end
 end
 reac = reac * 1e5;
+
+% calculate alphas
+nCoeff = nCondition;
+alpha = zeros(nPower,nCondition);
+for i = 1:nPower
+    % \alpha_{Power}
+    alpha(i,1) = (reac(i,2)-reac(i,1))/powerfudge;
+    % \alpha_{TE}
+    alpha(i,2) = (reac(i,3)-reac(i,1))/powerfudge;
+    % \alpha_{Tfuel} (Doppler)
+    alpha(i,3) = (reac(i,4)-reac(i,1))/fuelfudge;
+    % \alpha_{Tcool}
+    alpha(i,4) = (reac(i,5)-reac(i,1))/coolfudge;
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % plot keff figure
 figure
@@ -37,91 +57,77 @@ hold off
 xlim([0,power(end)])
 xlabel('% Power')
 ylabel('k_{eff}')
-title('Eigenvalue Effects')
+title('k_{eff} Effects')
 fuelstr = sprintf('T_{fuel}+%.0f[K]',fuelfudge);
 coolstr = sprintf('T_{cool}+%.0f[K]',coolfudge);
-legend_cells = {'All Feedback','No T.E.',fuelstr,coolstr};
+pwerstr = sprintf('Q_{Rx}+%.0f%%',powerfudge);
+thexpstr = sprintf('Thexp Q_{Rx}+%.0f%%',powerfudge);
+legend_cells = {'All Feedback',pwerstr,thexpstr,fuelstr,coolstr};
 legend(legend_cells)
 set(gca,'FontName',FN,'FontSize',FS);
-print(gcf,'../figs/eigenvalue_effects.eps','-depsc2')
+print(gcf,'../figs/keff_effects.eps','-depsc2')
 close(gcf)
 
 % plot reactivity effects
-figure
-hold on
-for i = 1:nCondition
-    h = plot(power,reac(:,i),'-o','LineWidth',LW);
-    set(h, 'MarkerFaceColor', get(h, 'Color'));
-end
-hold off
-xlim([0,power(end)])
-xlabel('% Power')
-ylabel('\rho [pcm]')
-title('Reactivity Effects')
-legend(legend_cells)
-set(gca,'FontName',FN,'FontSize',FS);
-print(gcf,'../figs/reactivity_effects.eps','-depsc2')
-close(gcf)
+% figure
+% hold on
+% for i = 1:nCondition
+%     h = plot(power,reac(:,i),'-o','LineWidth',LW);
+%     set(h, 'MarkerFaceColor', get(h, 'Color'));
+% end
+% hold off
+% xlim([0,power(end)])
+% xlabel('% Power')
+% ylabel('\rho [pcm]')
+% title('Reactivity Effects')
+% legend(legend_cells)
+% print(gcf,'reactivity_effects.png','-dpng')
+% close(gcf)
 
-% calculate alphas
-nCoeff = nCondition;
-alpha = zeros(nPower,nCondition);
-for i = 1:nPower
-    % \alpha_{Power}
-    alpha(i,1) = (reac(i,1)-reac(1,1))/(power(i)-power(1));
-    % \alpha_{TE}
-    alpha(i,2) = (reac(i,2)-reac(1,2))/(power(i)-power(1));
-    % \alpha_{Tfuel} (Doppler)
-    alpha(i,3) = -(reac(i,3)-reac(i,1))/fuelfudge;
-    % \alpha_{Tcool}
-    alpha(i,4) = -(reac(i,4)-reac(i,1))/coolfudge;
-end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         
 % plot coefficient figures
-figure
-h = plot(power,alpha(:,3),'-o','LineWidth',LW);
-set(h, 'MarkerFaceColor', get(h, 'Color'));
-xlabel('% Power')
-ylabel('\alpha_{Doppler} [pcm/K]')
-title('Doppler Coefficient')
-set(gca,'FontName',FN,'FontSize',FS);
-print(gcf,'../figs/alpha_doppler.eps','-depsc2')
-close(gcf)
 
-figure
-hold on
-h = plot(power,alpha(:,4),'-o','LineWidth',LW);
-set(h, 'MarkerFaceColor', get(h, 'Color'));
-plot([power(1),power(end)],[0,0],'k') % black line at zero
-hold off
-xlabel('% Power')
-ylabel('\alpha_{CTC} [pcm/K]')
-title('Coolant Temperature Coefficient')
-set(gca,'FontName',FN,'FontSize',FS);
-print(gcf,'../figs/alpha_ctc.eps','-depsc2')
-close(gcf)
-
-%%
-
-% plot coefficient figures
+% \alpha_{Power}
 figure
 h = plot(power,alpha(:,1),'-o','LineWidth',LW);
 set(h, 'MarkerFaceColor', get(h, 'Color'));
-xlabel('% Power')
-ylabel('\alpha_{Total} [pcm/%]')
-title('Power Coefficient')
-set(gca,'FontName',FN,'FontSize',FS);
-print(gcf,'../figs/alpha_total.eps','-depsc2')
+xlabel('Reactor Power [%]')
+ylabel('\alpha_{power} [pcm/%]')
+title('Power Reactivity Coefficient')
+set(gca,'FontName',FN,'FontSize',FS)
+print(gcf,'../figs/alpha_power.eps','-depsc2')
 close(gcf)
 
-
-% plot coefficient figures
+% \alpha_{thexp}
 figure
-h = plot(power,alpha(:,3),'-o','LineWidth',LW);
+h = plot(power,alpha(:,2),'-o','LineWidth',LW);
 set(h, 'MarkerFaceColor', get(h, 'Color'));
-xlabel('% Power')
-ylabel('\alpha_{Texp} [pcm/%]')
-title('Thermal Expansion Coefficient')
-set(gca,'FontName',FN,'FontSize',FS);
-print(gcf,'../figs/alpha_texp.eps','-depsc2')
+xlabel('Reactor Power [%]')
+ylabel('\alpha_{thexp} [pcm/%]')
+title('Thermal Expansion Reactivity Coefficient')
+set(gca,'FontName',FN,'FontSize',FS)
+print(gcf,'../figs/alpha_thexp.eps','-depsc2')
+close(gcf)
+
+% \alpha_{fuel}
+figure
+h = plot(temperature_avg(:,1),alpha(:,3),'-o','LineWidth',LW);
+set(h, 'MarkerFaceColor', get(h, 'Color'));
+xlabel('\overline{T_{fuel}} [K]')
+ylabel('\alpha_{Doppler} [pcm/K]')
+title('Doppler Reactivity Coefficient')
+set(gca,'FontName',FN,'FontSize',FS)
+print(gcf,'../figs/alpha_fuel.eps','-depsc2')
+close(gcf)
+
+% \alpha_{cool}
+figure
+h = plot(temperature_avg(:,3),alpha(:,4),'-o','LineWidth',LW);
+set(h, 'MarkerFaceColor', get(h, 'Color'));
+xlabel('\overline{T_{cool}} [K]')
+ylabel('\alpha_{CTC} [pcm/K]')
+title('Coolant Temperature Reactivity Coefficient')
+set(gca,'FontName',FN,'FontSize',FS)
+print(gcf,'../figs/alpha_cool.eps','-depsc2')
 close(gcf)
