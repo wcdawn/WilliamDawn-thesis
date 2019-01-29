ecacba885ea229865cc8d53491eb80c039eec87e
William Dawn <wcdawn@ncsu.edu>
2019-01-28 19:42:40 -0500

    fixing b.c. & font size

--- apB_benchmarks/matlab/problem_plotter.m
+++ apB_benchmarks/matlab/problem_plotter.m
@@ -4,11 +4,11 @@ close all
 % pseudo-input
 LW = 1;
 FN = 'Times New Roman';
-FS = 10;
+FS = 5;
 f2f = 1.0;
 
-fname = '../data/iaea_refl.csv';
-outname = '../figs/iaea_refl.eps';
+fname = '../data/hwr.csv';
+outname = '../figs/hwr.eps';
 
 side = 1.0/sqrt(3.0);
 base = 0.5*side;
@@ -90,22 +90,22 @@ end
 % text(7*f2f,7*f2f,'$$\alpha=0.5$$','Interpreter','latex');
 
 % HWR
-% key_x = 0.0;
-% key_y = 0.6*max(ring)*f2f;
-% plot(key_x+hex(:,1)*f2f,key_y+hex(:,2)*f2f,'k-','LineWidth',LW)
-% key_str = sprintf('Material\nPower');
-% text(key_x,key_y,key_str,'FontName',FN,'FontSize',FS,...
-%     'HorizontalAlignment','center','VerticalAlignment','middle');
-% eigenvalue_str = sprintf('k_{eff}=\n0.991965');
-% text(key_x,key_y*0.8,eigenvalue_str,'FontName',FN,'FontSize',FS+4,...
-%     'HorizontalAlignment','center','VerticalAlignment','middle');
-% arrow([key_x+0.8*f2f,key_y-0.5*f2f],[key_x+0.8*f2f,key_y+0.5*f2f],'Length',6,'Ends','both')
-% dimension_str = '17.78 [cm]';
-% text(key_x+f2f,key_y,dimension_str,'FontName',FN,'FontSize',FS+3);
-% bc_str = '$$\nabla \phi_g \cdot \hat{\mathbf{n}} = 0$$';
-% text(4*f2f,-1*f2f,bc_str,'Interpreter','latex');
-% text(0*f2f,6*f2f,bc_str,'Interpreter','latex');
-% text(12*f2f,12*f2f,'$$\phi_g = 0$$','Interpreter','latex')
+key_x = 0.0;
+key_y = 0.6*max(ring)*f2f;
+plot(key_x+hex(:,1)*f2f,key_y+hex(:,2)*f2f,'k-','LineWidth',LW)
+key_str = sprintf('Material\nPower');
+text(key_x,key_y,key_str,'FontName',FN,'FontSize',FS,...
+    'HorizontalAlignment','center','VerticalAlignment','middle');
+eigenvalue_str = sprintf('k_{eff}=\n0.991965');
+text(key_x,key_y*0.8,eigenvalue_str,'FontName',FN,'FontSize',FS+4,...
+    'HorizontalAlignment','center','VerticalAlignment','middle');
+arrow([key_x+0.8*f2f,key_y-0.5*f2f],[key_x+0.8*f2f,key_y+0.5*f2f],'Length',6,'Ends','both')
+dimension_str = '17.78 [cm]';
+text(key_x+f2f,key_y,dimension_str,'FontName',FN,'FontSize',FS+3);
+bc_str = sprintf('Rotational\nSymmetry');
+text(4*f2f,-1.5*f2f,bc_str,'FontName',FN,'FontSize',FS+3,'HorizontalAlignment','center')
+text(1*f2f,6*f2f,bc_str,'FontName',FN,'FontSize',FS+3,'HorizontalAlignment','center')
+text(12*f2f,12*f2f,'$$\phi_g = 0$$','Interpreter','latex')
 
 % IAEA_NORE
 % key_x = 0.0;
@@ -127,23 +127,23 @@ end
 % text(4*f2f,5*f2f,out_bc_str,'FontName',FN,'FontSize',FS);
 
 % IAEA_REFL
-key_x = 0.0;
-key_y = 0.6*max(ring)*f2f;
-plot(key_x+hex(:,1)*f2f,key_y+hex(:,2)*f2f,'k-','LineWidth',LW)
-key_str = sprintf('Material\n\\alpha=0.125\n\\alpha=0.5');
-text(key_x,key_y,key_str,'FontName',FN,'FontSize',FS,...
-    'HorizontalAlignment','center','VerticalAlignment','middle');
-eigenvalue_str = sprintf('k_{eff}=\n1.006630 \\alpha=0.125\n1.005507 \\alpha=0.50');
-text(key_x,key_y*0.7,eigenvalue_str,'FontName',FN,'FontSize',FS+1,...
-    'HorizontalAlignment','center','VerticalAlignment','middle');
-arrow([key_x+0.8*f2f,key_y-0.5*f2f],[key_x+0.8*f2f,key_y+0.5*f2f],'Length',6,'Ends','both')
-dimension_str = '20.00 [cm]';
-text(key_x+f2f*.9,key_y,dimension_str,'FontName',FN,'FontSize',FS+1);
-bc_str = '$$\nabla \phi_g \cdot \hat{\mathbf{n}} = 0$$';
-text(4*f2f,1*f2f,bc_str,'Interpreter','latex');
-text(-1*f2f,1.5*f2f,bc_str,'Interpreter','latex');
-out_bc_str = sprintf('\\alpha=0.125 \n \\alpha=0.5');
-text(4*f2f,5.5*f2f,out_bc_str,'FontName',FN,'FontSize',FS);
+% key_x = 0.0;
+% key_y = 0.6*max(ring)*f2f;
+% plot(key_x+hex(:,1)*f2f,key_y+hex(:,2)*f2f,'k-','LineWidth',LW)
+% key_str = sprintf('Material\n\\alpha=0.125\n\\alpha=0.5');
+% text(key_x,key_y,key_str,'FontName',FN,'FontSize',FS,...
+%     'HorizontalAlignment','center','VerticalAlignment','middle');
+% eigenvalue_str = sprintf('k_{eff}=\n1.006630 \\alpha=0.125\n1.005507 \\alpha=0.50');
+% text(key_x,key_y*0.7,eigenvalue_str,'FontName',FN,'FontSize',FS+1,...
+%     'HorizontalAlignment','center','VerticalAlignment','middle');
+% arrow([key_x+0.8*f2f,key_y-0.5*f2f],[key_x+0.8*f2f,key_y+0.5*f2f],'Length',6,'Ends','both')
+% dimension_str = '20.00 [cm]';
+% text(key_x+f2f*.9,key_y,dimension_str,'FontName',FN,'FontSize',FS+1);
+% bc_str = '$$\nabla \phi_g \cdot \hat{\mathbf{n}} = 0$$';
+% text(4*f2f,1*f2f,bc_str,'Interpreter','latex');
+% text(-1*f2f,1.5*f2f,bc_str,'Interpreter','latex');
+% out_bc_str = sprintf('\\alpha=0.125 \n \\alpha=0.5');
+% text(4*f2f,5.5*f2f,out_bc_str,'FontName',FN,'FontSize',FS);
 
 hold off
 axis equal
