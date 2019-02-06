681a4133f902ffeff30cc1df418fcac66e566327
William Dawn <wdawn730@gmail.com>
2019-02-06 14:38:26 -0500

    added zeros of thermal expansion coefficients

--- ch05_thermalExpansion/matlab/lef_plot.m
+++ ch05_thermalExpansion/matlab/lef_plot.m
@@ -1,13 +1,17 @@
 clear
 close all
 
-fname = '../data/lef.csv';
-data = csvread(fname,1,0);
+lef_ht9 = @(T)(-2.191e-3 + 5.678e-6*T + 8.111e-9*T.^2 + -2.576e-12*T.^3);
+lef_fuel_lo = @(T)(-7.3e-3 + 3.489e-5*T - 5.154e-8*T.^2 + 4.39e-11*T.^3);
+lef_fuel_hi = @(T)(-0.25252 + 6.669e-4*T - 5.441e-7*T.^2 + 1.518e-10*T.^3);
+lef_fuel = @(T)(lef_fuel_lo(T)+heaviside(T-923).*(lef_fuel_hi(T)-lef_fuel_lo(T)));
+
+fprintf('lef_ht9==0 = %.6f\n',fzero(lef_ht9,200.));
+fprintf('lef_fuel==0 = %.6f\n',fzero(lef_fuel,200.));
+
 LW = 2;
 
-T = data(:,1);
-lef_fuel  = data(:,2);
-lef_steel = data(:,3);
+T = linspace(400,1200,1000);
 
 ndot = 10;
 ntest = length(T);
@@ -17,16 +21,18 @@ selected = floor(1:delta_selected:ntest);
 matlab_blue = [0.00 0.447 0.741];
 matlab_orange = [0.91 0.41 0.17];
 
+lef_fuel_data = lef_fuel(T);
+
 figure
 hold on
-plot(T,lef_fuel ,'-o','Color',matlab_blue,'LineWidth',LW,'MarkerIndices',selected,'MarkerFaceColor',matlab_blue)
-plot(T,lef_steel,'-^','Color',matlab_orange,'LineWidth',LW,'MarkerIndices',selected,'MarkerFaceColor',matlab_orange)
+plot(T,lef_fuel_data,'-o','Color',matlab_blue,'LineWidth',LW,'MarkerIndices',selected,'MarkerFaceColor',matlab_blue)
+plot(T,lef_ht9(T),'-^','Color',matlab_orange,'LineWidth',LW,'MarkerIndices',selected,'MarkerFaceColor',matlab_orange)
 hold off
 legend({'LEF U10Zr','LEF HT9'},'Location','NorthWest')
 xlabel('Temperature [K]')
 ylabel('Linear Expansion Factor (LEF)')
 xlim([T(1),T(end)])
-ylim([0,max(lef_fuel)*1.05])
+ylim([0,max(lef_fuel_data)*1.05])
 ax = gca;
 ax.YAxis.Exponent=-2; % this is the same as percentage results
 set(gca,'FontName','Times New Roman')
