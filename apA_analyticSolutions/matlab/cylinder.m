8e4c5ff31cc2a68885ba55a881600f05958e1a13
William Dawn <wcdawn@ncsu.edu>
2019-02-13 11:29:32 -0500

    improved quality of figures

--- apA_analyticSolutions/matlab/cylinder.m
+++ apA_analyticSolutions/matlab/cylinder.m
@@ -3,25 +3,24 @@ close all
 
 T = 1;
 H = 2;
-% NOTE: if this is too large the image will be saved as a bitmap automatically
-ntest = 76; 
+ntest = 100; 
 
 alpha_0 = fzero(@(z) besselj(0, z), 3);
 % besselj(0, alpha_0); % Should be small
 
-fun = @(r,z)(besselj(0,(alpha_0/T)*r)*sin((pi/H)*z));
+fun = @(r,z)(besselj(0,(alpha_0/T)*r).*sin((pi/H)*z));
 
-rtest = linspace(0,T,ntest);
-ztest = linspace(0,H,ntest);
-ftest = zeros(length(rtest),length(ztest));
-for i = 1:ntest
-    for j = 1:ntest
-        ftest(i,j) = fun(rtest(i),ztest(j));
-    end
-end
+dr = T/ntest;
+dz = H/ntest;
+r = 0.5*dr:dr:T;
+z = 0.5*dz:dz:H;
+
+[R,Z] = meshgrid(r,z);
+
+val = fun(R,Z)';
 
 figure('pos',[1000 1000 650 1000]);
-surf(rtest,ztest,ftest','EdgeColor','flat');
+imagesc(r,z,val');
 view([0,90]);
 colormap(viridis);
 colorbar;
